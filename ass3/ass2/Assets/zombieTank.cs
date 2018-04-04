5ac9f9d4f25373856d3212d06f24d55bd9c20949
pakman888 <patrick.ph.chan@gmail.com>
2018-04-04 12:14:14 -0400

    latest- working on AI

--- ass3/ass2/Assets/zombieTank.cs
+++ ass3/ass2/Assets/zombieTank.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class zombieTank : MonoBehaviour {
 
+    public GameObject [] path = new GameObject[13];
+
 	// Use this for initialization
 	void Start () {
-		
+        transform.position = path[0].transform.position;
 	}
 	
 	// Update is called once per frame
